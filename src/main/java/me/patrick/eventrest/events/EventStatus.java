766ed880816a7fdc4dec775bf79f9de88e44059b
patrickYoo <yhy1045@naver.com>
2022-02-22 22:49:42 +0900

    restApi push

---
+++ src/main/java/me/patrick/eventrest/events/EventStatus.java
@@ -0,0 +1,8 @@
+package me.patrick.eventrest.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED
+
+}
+
---
+++ src/test/java/me/patrick/eventrest/EventRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.patrick.eventrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
